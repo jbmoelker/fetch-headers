43540a89e7096a7ef0c39e840c2e0619a39eb250
Jasper Moelker <j.b.moelker@activeids.nl>
2017-11-16 22:19:40 +0100

    Add tests for all error scenarios

--- test.js
+++ test.js
@@ -25,3 +25,48 @@ test('returns headers', async t => {
     mockSite.close()
     service.close()
 })
+
+test('returns `400` `MISSING_PARAMETER` if request has no `url` parameter', async t => {
+    // use service on mock site:
+    const service = micro(fetchHeaders)
+    const url = await listen(service)
+    const response = await request({
+        method: 'GET',
+        uri: `${url}/`,
+        resolveWithFullResponse: true,
+        simple: false,
+    })
+    const { errors } = JSON.parse(response.body)
+
+    // assert service response:
+    t.deepEqual(response.statusCode, 400)
+    t.deepEqual(errors[0].code, 'MISSING_PARAMETER')
+
+    // clean up:
+    service.close()
+})
+
+test('returns `500` `FETCH_ERROR` if web page is not found', async t => {
+    // setup mock site to use the service on:
+    const mockSite = micro(async (req, res) => micro.send(res, 404, {}))
+    const mockSiteUrl = await listen(mockSite)
+
+    // use service on mock site:
+    const service = micro(fetchHeaders)
+    const url = await listen(service)
+    const response = await request({
+        method: 'GET',
+        uri: `${url}/?url=${mockSiteUrl}`,
+        resolveWithFullResponse: true,
+        simple: false,
+    })
+    const { errors } = JSON.parse(response.body)
+
+    // assert service response:
+    t.deepEqual(response.statusCode, 500)
+    t.deepEqual(errors[0].code, 'FETCH_ERROR')
+
+    // clean up:
+    mockSite.close()
+    service.close()
+})
